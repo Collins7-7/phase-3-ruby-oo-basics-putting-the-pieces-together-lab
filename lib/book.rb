8db7b8793d52643aca1c83a9382de8987e88b053
COllins <collinsmuiruri77@gmail.com>
2022-10-20 14:55:27 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
+
+    attr_accessor :page_count
+    attr_accessor :genre
+
+    def initialize (title)
+        @title = title
+    end 
+
+    def title=(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
 
+end
+
+
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
+book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,37 @@
-# Make your shoe class here!
+class Shoe
+
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand=(name)
+        @brand = name
+    end
+
+    def brand 
+        @brand
+    end
+
+    def condition=(value)
+        @condition = value
+    end
+
+    def condition
+        @condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+shoe= Shoe.new("Nike")
+shoe.brand 
+shoe.color = "red"
+shoe.size = 9.5
+shoe.material = "suede"
+shoe.condition = "tattered"
